a6d9f1c694e1d99c3c5dd6c8cf41ef1d50c1222a
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-16 10:11:53 +0900

    Feat: Create MainView and set custom colors

---
+++ MC2-Team3-Pilling/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  MC2-Team3-Pilling
+//
+//  Created by Groo on 5/16/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
