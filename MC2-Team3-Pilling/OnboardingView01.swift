bc6171d367d00781b32a6efdb45c9ec8c4e976c2
Lee Sohyeon <teabag759@gmail.com>
2024-05-16 17:31:25 +0900

    Drawing OnboardingView (1)

--- MC2-Team3-Pilling/OnboardingView01.swift
+++ MC2-Team3-Pilling/OnboardingView01.swift
@@ -1,15 +1,71 @@
-//
-//  OnboardingView01.swift
-//  MC2-Team3-Pilling
-//
-//  Created by 이소현 on 5/16/24.
-//
 
 import SwiftUI
 
 struct OnboardingView01: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image("making-plan")
+            .resizable()
+            .frame(width: 300, height: 300)
+        
+        // Text
+        VStack {
+            // Text
+                Text("플랜을 설정해주세요!")
+                    .font(.title)
+                    .bold()
+            
+                Text("설정은 추후에 변경 가능합니다.")
+                    .font(.title3)
+                    .foregroundStyle(.secondary)
+
+            }
+        
+        //        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+        //        .background(Color.red)
+        
+        // Selecting box
+        VStack {
+            Button(action: {}, label: {
+                // sfSymbol 부재 : medicine-bottle-one
+                Image(systemName: "pill.circle.fill")
+                                    Text("약 종류")
+                    .font(.title3)
+            })
+            .padding(.vertical, 20)
+            .frame(maxWidth: .infinity)
+            .background(.customGray)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .foregroundColor(.secondary)
+            .padding()
+            
+            Button(action: {}, label: {
+                // sfSymbol 부재 : uis-calender
+                Image(systemName: "note")
+                Text("현재 복용 일수")
+                    .font(.title3)
+            })
+            .padding(.vertical, 20)
+            .frame(maxWidth: .infinity)
+            .background(.customGray)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .foregroundColor(.secondary)
+            .padding()
+
+        }
+        
+        
+        // footer button
+        Button(action: {}, label: {
+            Text("다음으로")
+                .font(.title3)
+                .bold()
+        })
+        .padding(.vertical, 30)
+        .frame(maxWidth: .infinity)
+        .background(.customGreen)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        .foregroundColor(.black)
+        .padding()
     }
 }
 
