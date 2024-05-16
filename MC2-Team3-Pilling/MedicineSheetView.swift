02980d4fc6e75872278b8c40839d6ee7f91d2d24
Lee Sohyeon <teabag759@gmail.com>
2024-05-17 06:49:59 +0900

    OnboardingView (2)

---
+++ MC2-Team3-Pilling/MedicineSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  MedicineSheetView.swift
+//  MC2-Team3-Pilling
+//
+//  Created by 이소현 on 5/17/24.
+//
+
+import SwiftUI
+
+struct MedicineSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MedicineSheetView()
+}
--- MC2-Team3-Pilling/OnboardingView01.swift
+++ MC2-Team3-Pilling/OnboardingView01.swift
@@ -2,6 +2,8 @@
 import SwiftUI
 
 struct OnboardingView01: View {
+    @State private var showingMedicineSheet = false
+    
     var body: some View {
         Image("making-plan")
             .resizable()
@@ -10,47 +12,55 @@ struct OnboardingView01: View {
         // Text
         VStack {
             // Text
-                Text("플랜을 설정해주세요!")
-                    .font(.title)
-                    .bold()
+            Text("플랜을 설정해주세요!")
+                .font(.title)
+                .bold()
             
-                Text("설정은 추후에 변경 가능합니다.")
-                    .font(.title3)
-                    .foregroundStyle(.secondary)
-
-            }
+            Text("설정은 추후에 변경 가능합니다.")
+                .font(.title3)
+                .foregroundStyle(.secondary)
+            
+        }
         
         //        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
         //        .background(Color.red)
         
         // Selecting box
         VStack {
-            Button(action: {}, label: {
+            Button(action: {
+                self.showingMedicineSheet = true
+            }, label: {
                 // sfSymbol 부재 : medicine-bottle-one
                 Image(systemName: "pill.circle.fill")
-                                    Text("약 종류")
+                Text("약 종류")
                     .font(.title3)
+                Image(systemName: "chevron.right")
             })
             .padding(.vertical, 20)
             .frame(maxWidth: .infinity)
             .background(.customGray)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
             .foregroundColor(.secondary)
             .padding()
+            .sheet(isPresented: $showingMedicineSheet){
+                MedicineSheetView()
+                    .presentationDetents([.medium])
+            }
             
             Button(action: {}, label: {
                 // sfSymbol 부재 : uis-calender
                 Image(systemName: "note")
                 Text("현재 복용 일수")
                     .font(.title3)
+                
             })
             .padding(.vertical, 20)
             .frame(maxWidth: .infinity)
             .background(.customGray)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
             .foregroundColor(.secondary)
             .padding()
-
+            
         }
         
         
