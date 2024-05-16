00085bf4fd532198404351248e22dc7202c96bce
Lee Sohyeon <teabag759@gmail.com>
2024-05-16 14:10:10 +0900

    OnboardingView and SettingView

---
+++ MC2-Team3-Pilling/OnboardingView01.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView01.swift
+//  MC2-Team3-Pilling
+//
+//  Created by 이소현 on 5/16/24.
+//
+
+import SwiftUI
+
+struct OnboardingView01: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView01()
+}
---
+++ MC2-Team3-Pilling/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  MC2-Team3-Pilling
+//
+//  Created by 이소현 on 5/16/24.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
