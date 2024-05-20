b07791b0641f5ac23dc68ae2de2284b698b4c402
piri <pyoram25@gmail.com>
2024-05-21 00:08:32 +0900

    Refactor: 온보딩 페이지 에러 수정

--- MC2-Team3-Pilling/View/OnboardingFirstView.swift
+++ MC2-Team3-Pilling/View/OnboardingFirstView.swift
@@ -6,7 +6,6 @@ struct OnboardingFirstView: View {
     
     
     var body: some View {
-
         Image("making-plan")
             .resizable()
             .frame(width: 240, height: 240)
@@ -28,19 +27,13 @@ struct OnboardingFirstView: View {
                 self.showingMedicineSheet = true
             }, label: {
                 // sfSymbol 부재 : medicine-bottle-one
-
                 HStack {
-                    VStack(alignment: .leading) {
-                        Text("복용하고 계신 약을 알려주세요!")
-                            .largeTitle()
-                            .padding(.bottom, 2)
-                        
-                        Text("설정은 추후에 변경 가능합니다.")
-                            .secondaryRegular()
-                    }
+                    Image(systemName: "pill.circle.fill")
+                    Text("약 종류")
+                        .secondaryTitle()
                     Spacer()
+                    Image(systemName: "chevron.right")
                 }
-
                 .padding([.leading, .trailing], 25)
             })
             .padding(.vertical, 20)
@@ -64,16 +57,33 @@ struct OnboardingFirstView: View {
                     Text("현재 복용 일수")
                         .secondaryTitle()
                     Spacer()
-
                 }
-                .padding(.vertical, 30)
-                .frame(maxWidth: .infinity)
-                .background(.customGreen)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .foregroundColor(.black)
-            }
-            .padding()
+                .padding([.leading, .trailing], 25)
+            })
+            .padding(.vertical, 20)
+            .frame(maxWidth: .infinity)
+            .background(.customGray02)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .foregroundColor(.secondary)
+            .padding([.leading, .trailing], 20)
+            
         }
+        
+        Spacer()
+        
+        // footer button
+        Button(action: {
+//            OnboardingView02()
+        }, label: {
+            Text("다음으로")
+                .largeBold()
+        })
+        .padding(.vertical, 30)
+        .frame(maxWidth: .infinity)
+        .background(.customGreen)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        .foregroundColor(.black)
+        .padding()
     }
 }
 
