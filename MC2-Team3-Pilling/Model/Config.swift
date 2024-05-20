5bcb8c9178cc6a4870fc5a8405bcfb88fdeab42c
piri <pyoram25@gmail.com>
2024-05-20 22:35:43 +0900

    Feat: Config 파일에 복용상태와 상태안내창을 관리하는 enum 생성

---
+++ MC2-Team3-Pilling/Model/Config.swift
@@ -0,0 +1,41 @@
+//
+//  Config.swift
+//  MC2-Team3-Pilling
+//
+//  Created by ram on 5/20/24.
+//
+
+import Foundation
+
+class Config{
+    enum IntakeStatus: Int {
+        case notYet = 0
+        case onePill = 1
+        case twoPills = 2
+        case placebo = 3
+    }
+    enum StatusMessage: Int, CustomStringConvertible { //문구 변경 예정
+        case plantGrass = 0
+        case limitTwoHours = 1
+        case plantTwoGrass = 2
+        case grassGrowingWell = 3
+        case notRecording = 4
+        
+        var description: String {
+            switch self {
+            case .plantGrass:
+                return "잔디를 심어주세요"
+            case .limitTwoHours:
+                return "잔디는 2시간을 초과하지 않게 심어주세요!"
+            case .plantTwoGrass:
+                return "2개의 잔디를 심어주세요"
+            case .grassGrowingWell:
+                return "잔디가 잘 자라고 있어요!"
+            case .notRecording:
+                return "기록을 안하고 계신가요?"
+            }
+        }
+    }
+
+
+}
