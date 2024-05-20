7d01ca1ffaadd168a5d751817f8de57bfd70b1d2
Lee Sohyeon <teabag759@gmail.com>
2024-05-21 00:15:21 +0900

    Feat: PopoverView 생성 및 MainView 연결 - PopoverView 생성함 - "info.circle.fill" 클릭 시 PopoverView가 나타나도록 연결

---
+++ MC2-Team3-Pilling/View/PopoverView.swift
@@ -0,0 +1,18 @@
+//
+//  PopoverView.swift
+//  MC2-Team3-Pilling
+//
+//  Created by 이소현 on 5/21/24.
+//
+
+import SwiftUI
+
+struct PopoverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopoverView()
+}
