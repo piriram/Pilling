7854f4b2fb770a48caadfdf844fcf37d561549e6
Lee Sohyeon <teabag759@gmail.com>
2024-05-21 20:16:07 +0900

    Feat: enum DosageType 생성 및 picker에 적용

--- MC2-Team3-Pilling/View/ChooseStatusView.swift
+++ MC2-Team3-Pilling/View/ChooseStatusView.swift
@@ -7,8 +7,28 @@
 
 import SwiftUI
 
+// 복용 여부 enum
+enum DosageType: Int, CaseIterable, Identifiable {
+    case notYet = 0
+    case onePill = 1
+    case twoPills = 2
+    
+    var id: Self { self }
+    
+    var takingType: String {
+        switch self {
+        case .notYet:
+            "미복용"
+        case .onePill:
+            "복용"
+        case .twoPills:
+            "2알 복용"
+        }
+    }
+}
+
 struct ChooseStatusView: View {
-    @State private var selectedNum = 0
+//    @State private var selectedNum = 0
     @State private var takeMedicineTime: Date = Date()
     
     // 부작용 섹션 vars
@@ -16,6 +36,8 @@ struct ChooseStatusView: View {
     @State private var nauseaToggle = false
     @State private var swellingToggle = false
     @State var sideEffectMemo: String = ""
+    
+    @State private var dosageType: DosageType = .notYet
     
     
     var body: some View {
@@ -29,12 +51,13 @@ struct ChooseStatusView: View {
             }
             
             // 약 복용 여부 확인
-            Picker("selection", selection: $selectedNum) {
-                ForEach(1..<4) { num in
-                    Text("\(num)")
+            Picker("selection", selection: $dosageType) {
+                ForEach(DosageType.allCases) { dosageType in
+                    Text("\(dosageType.takingType)").tag(dosageType.rawValue)
                 }
             }
             .pickerStyle(.segmented)
+            .padding()
             // selection Print 되는 값 확인
 //            .onChange(of: selectedNum) { newValue in
 //                print("Selected number: \(newValue)")
@@ -63,14 +86,15 @@ struct ChooseStatusView: View {
             .foregroundColor(.secondary)
             .padding()
             
+            
             // 부작용 섹션
             VStack {
                 Toggle("부정출혈", isOn: $irrBleedingToggle)
-                    .padding([.leading, .trailing], 20)
+                    
                 Toggle("구역질", isOn: $nauseaToggle)
-                    .padding([.leading, .trailing], 20)
+                    
                 Toggle("붓기", isOn: $swellingToggle)
-                    .padding([.leading, .trailing], 20)
+                    
                 
                 HStack {
                     Text("메모")
@@ -80,8 +104,8 @@ struct ChooseStatusView: View {
                         .background(Color(uiColor: .secondarySystemBackground))
                     
                 }
-                .padding()
             }
+            .padding()
             
             
             // footer button
