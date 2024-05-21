ef6dd163b3c9a4668b4d12b69e39cedc2778cc4a
piri <pyoram25@gmail.com>
2024-05-21 22:35:43 +0900

    Feat: 온보딩퍼스트뷰를 파이리뷰로 만들어서 테스트중

---
+++ MC2-Team3-Pilling/View/PiriView.swift
@@ -0,0 +1,18 @@
+//
+//  PiriView.swift
+//  MC2-Team3-Pilling
+//
+//  Created by ram on 5/21/24.
+//
+
+import SwiftUI
+
+struct PiriView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PiriView()
+}
