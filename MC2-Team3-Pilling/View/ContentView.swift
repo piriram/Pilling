0c6d4f15cce5d4a0b28e014533b1e3e0326c3230
Lee Sohyeon <teabag759@gmail.com>
2024-05-21 16:25:26 +0900

    Fix : LiveActivity 화면에 나타나지 않는 문제 해결 1. MainView 의 start live activity의 stale date : nil로 변경 2. LiveTimeWidget의 ios Deployment target 17.4 -> 17.2로 변경

--- MC2-Team3-Pilling/View/ContentView.swift
+++ MC2-Team3-Pilling/View/ContentView.swift
@@ -51,7 +51,7 @@ struct ContentView: View {
                         // start live activity
                         let attributes = LiveTimeAttributes()
                         let state = LiveTimeAttributes.ContentState(restOfTime: alarmTime)
-                        let content = ActivityContent<LiveTimeAttributes.ContentState>(state: state, staleDate: alarmTime.addingTimeInterval(600))
+                        let content = ActivityContent<LiveTimeAttributes.ContentState>(state: state, staleDate: nil /*alarmTime.addingTimeInterval(600)*/)
                         activity = try? Activity<LiveTimeAttributes>.request(attributes: attributes, content: content, pushType: nil)
                     } else {
                         // end live activity
